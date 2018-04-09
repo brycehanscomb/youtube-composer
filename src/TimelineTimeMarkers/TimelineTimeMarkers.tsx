e1a292cf1dcdc82f5185720af57c7dc3e0857fa5
Bryce Hanscomb <b.hanscomb@gmail.com>
2018-04-09 21:06:07 +1000

    remove unused dependency

--- src/TimelineTimeMarkers/TimelineTimeMarkers.tsx
+++ src/TimelineTimeMarkers/TimelineTimeMarkers.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { range } from 'lodash';
-import seconds2timecode from 'seconds2timecode';
 
 import './style.css';
 
@@ -10,7 +9,7 @@ export default function TimelineTimeMarkers(props : any) {
             {range(0, 10, 1).map(markerTime => {
                 return (
                     <span key={markerTime} style={{left: `${markerTime * 100}px`}} className="Timeline__TimeMarker">
-                        {seconds2timecode(markerTime,3)}
+                        00:0{markerTime}
                     </span>
                 );
             })}
