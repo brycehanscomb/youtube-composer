1a7c6676536333a140d65c4423c7f4e4c2eec7c8
Bryce Hanscomb <b.hanscomb@gmail.com>
2018-04-14 12:30:06 +1000

    work on getting correct time markers

--- src/TimelineTimeMarkers/TimelineTimeMarkers.tsx
+++ src/TimelineTimeMarkers/TimelineTimeMarkers.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { range } from 'lodash';
 
 import './style.css';
+import {getWidth} from "../utils";
 
 export default function TimelineTimeMarkers(props : any) {
+    const widthOfSecond = getWidth(1000, props.zoom);
     return (
         <>
-            {range(0, 10, 1).map(markerTime => {
+            {range(0, 20, 1).map((markerTime, index) => {
                 return (
-                    <span key={markerTime} style={{left: `${markerTime * 100}px`}} className="Timeline__TimeMarker">
-                        00:0{markerTime}
+                    <span key={markerTime} style={{left: `${widthOfSecond * index}px`}} className="Timeline__TimeMarker">
+                        00:{markerTime > 9 ? markerTime : '0' + markerTime}
                     </span>
                 );
             })}
